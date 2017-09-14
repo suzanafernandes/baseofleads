5be78a4d90d390112f9dd827618fb6234765a5b5
Suzana <suzanafernandes@suzanafernandes.com>
2017-09-13 22:01:20 -0300

    Create segmentation_leads_steps.rb

---
+++ features/step_definitions/segmentation_leads_steps.rb
@@ -0,0 +1,18 @@
+Feature: Segmentation of Leads
+Scenario:
+  Given(/^navigate to page Segmentation of Leads$/) do
+    When(/^We click on 'Segmentacao de Leads'$/) do
+      click_link 'Segmentação de Leads'
+  end
+    When(/^We click on 'Criar Segmentacao'$/) do
+      click_link 'Criar Segmentação'
+    end
+    And(/^fill the segmentation and save$/) do
+      fill_in('segmentation_name', :with => 'Marketing')
+      page.all(:css, 'select.form-control.input-sm.js-input')[0].all(:css, 'option')[1].select_option
+      click_button 'Salvar'
+    end
+Then(/^We view the segmentation registered$/) do
+    expect(page).to have_content "Marketing"
+end
+  
